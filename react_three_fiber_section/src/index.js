a6c537b5d3e236db79d2fc4cb135c50aaefdd0df
Ziad-Asr <ziadelsayedasr@gmail.com>
2025-04-19 14:21:50 +0200

    Resturcture the files in react three fiber module

--- react_three_fiber_section/src/index.js
+++ react_three_fiber_section/src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import "./styles.css";
-import Scene from "./Scene";
+import Scene from "./studied/Scene_12";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
